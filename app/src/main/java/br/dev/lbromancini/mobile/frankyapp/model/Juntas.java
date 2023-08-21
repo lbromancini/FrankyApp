06f4f34265e94ab28598cf6ade20fe49ac5a616f
lbromancini <lbromancini@gmail.com>
2023-08-21 17:08:38 -0300

    07.08.2023 aula 02 tela de cadastro

---
+++ app/src/main/java/br/dev/lbromancini/mobile/frankyapp/model/Juntas.java
@@ -0,0 +1,61 @@
+package br.dev.lbromancini.mobile.frankyapp.model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Juntas {
+
+    // atributos
+    private String nomePonto;
+    private int angulo_junta1;
+    private int angulo_junta2;
+    private int angulo_junta3;
+
+    // métodos
+    public String getNomePonto() {return nomePonto;}
+    public void setNomePonto(String nomePonto) {this.nomePonto = nomePonto;}
+    public int getAngulo_junta1() {return angulo_junta1;}
+    public void setAngulo_junta1(int angulo_junta1) {this.angulo_junta1 = angulo_junta1;}
+    public int getAngulo_junta2() {return angulo_junta2;}
+    public void setAngulo_junta2(int angulo_junta2) {this.angulo_junta2 = angulo_junta2;}
+    public int getAngulo_junta3() {return angulo_junta3;}
+    public void setAngulo_junta3(int angulo_junta3) {this.angulo_junta3 = angulo_junta3;}
+
+    public Juntas(JSONObject jp) {
+        try {
+            this.setNomePonto((String) jp.get("nomePonto"));
+
+            Integer numero = (int) jp.get("angulo_junta1");
+            this.setAngulo_junta1(numero);
+
+            numero = (int) jp.get("angulo_junta2");
+            this.setAngulo_junta2(numero);
+
+            numero = (int) jp.get("angulo_junta3");
+            this.setAngulo_junta3(numero);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Juntas() {
+        this.setNomePonto("");
+        this.setAngulo_junta1(0);
+        this.setAngulo_junta2(0);
+        this.setAngulo_junta3(0);
+    }
+
+    public JSONObject toJsonObject() {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("nomePonto", this.nomePonto);
+            json.put("angulo_junta1", this.angulo_junta1);
+            json.put("angulo_junta2", this.angulo_junta2);
+            json.put("angulo_junta3", this.angulo_junta3);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+}
+
--- app/src/main/java/br/dev/lbromancini/mobile/frankyapp/ui/controle/ValorJuntasFragment.java
+++ app/src/main/java/br/dev/lbromancini/mobile/frankyapp/ui/controle/ValorJuntasFragment.java
@@ -7,15 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import br.dev.lbromancini.mobile.frankyapp.R;
+import br.dev.lbromancini.mobile.frankyapp.model.Juntas;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ValorJuntasFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ValorJuntasFragment extends Fragment {
+public class ValorJuntasFragment extends Fragment implements View.OnClickListener{
+
+    private View view = null;
+
+    private EditText nomePonto;
+    private EditText angulo_junta1;
+    private EditText angulo_junta2;
+    private EditText angulo_junta3;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +65,37 @@ public class ValorJuntasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_valor_juntas, container, false);
+        this.view = inflater.inflate(R.layout.fragment_valor_juntas, container, false);
+        // Binding
+        this.nomePonto = (EditText) view.findViewById(R.id.etNomePonto);
+        this.angulo_junta1 = (EditText) view.findViewById(R.id.ndAngulo_junta1);
+        this.angulo_junta2 = (EditText) view.findViewById(R.id.ndAngulo_junta2);
+        this.angulo_junta3 = (EditText) view.findViewById(R.id.ndAngulo_junta3);
+
+        return this.view;
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.btRealizarMov:
+
+                Juntas valorJuntas = new Juntas();
+
+                valorJuntas.setNomePonto(this.nomePonto.getText().toString());
+
+                String temp = this.angulo_junta1.getText().toString();
+                valorJuntas.setAngulo_junta1(Integer.valueOf(temp));
+
+                temp = this.angulo_junta2.getText().toString();
+                valorJuntas.setAngulo_junta2(Integer.valueOf(temp));
+
+                temp = this.angulo_junta3.getText().toString();
+                valorJuntas.setAngulo_junta3(Integer.valueOf(temp));
+
+                break;
+        }
     }
 }
